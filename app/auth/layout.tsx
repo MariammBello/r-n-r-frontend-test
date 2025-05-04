32234666ce0e6ec4248e1e72117b697275711c65
mariammbello <sheactuary@gmail.com>
2025-05-04 18:29:22 +0100

    update display on signup

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -11,20 +11,20 @@ export default function AuthLayout({
       {/* Left side - Shared Branding Panel */}
       <div className="w-full sm:w-1/2 relative hidden sm:block">
         <Image
-          src="/Images/flightimage.png" // Using /Images/ path
+          src="/images/flightimage.png" // Using /images/ path
           alt="Aerial view with world map"
           fill
           className="object-cover"
         />
         <Image
-          src="/Images/plane.png" // Using /Images/ path
+          src="/images/plane.png" // Using /images/ path
           alt="plane"
           width={800}
           height={100}
           className="absolute top-20 left-20 z-10"
         />
         <Image
-          src="/Images/logo2.png" // Using /Images/ path
+          src="/images/logo2.png" // Using /images/ path
           alt="roots n routes logo"
           width={200}
           height={50}
